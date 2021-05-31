063a7de3224e10132b23db8155c245974c2202b1
Fionn Fuchs <fionn.fuchs@gmail.com>
2021-05-31 16:55:05 +0200

    Remove unused index route

--- backend/routes/view.js
+++ backend/routes/view.js
@@ -4,12 +4,16 @@ var express = require('express');
 function viewRoute(databaseService) {
   var router = express.Router();
   router.get('/', async (req, res, next) => {
+
     const user = req.query.user;
     const token = req.query.token;
+
     console.log("Trying to get view data for user " + user + " with token " + token);
-    var diaryEntries = await databaseService.getWebViewData(user, token);
-    console.log(diaryEntries);
-    res.send(diaryEntries);
+    const viewData = await databaseService.getWebViewData(user, token);
+    console.log(viewData);
+
+    res.send(viewData);
+
   });
   return router;
 }
